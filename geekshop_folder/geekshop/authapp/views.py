51245ec02f5e2bd231d4c5824aaef57b3e195302
Hellica-Ordo <mittraunuruodo@gmail.com>
2022-01-20 23:40:28 +0300

    level2 homework 1

--- geekshop_folder/geekshop/authapp/views.py
+++ geekshop_folder/geekshop/authapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 from django.contrib import auth
 from django.urls import reverse
+from django.core.mail import send_mail
+from django.conf import settings
+from authapp.models import ShopUser
 
 
 def login(request):
@@ -34,21 +37,52 @@ def logout(request):
     return HttpResponseRedirect(reverse('index'))
 
 
+def send_verify_mail(user):
+    verify_link = reverse('auth:verify', args=[user.email, user.activation_key])
+
+    title = f'Подтверждение учётной записи - {user.username}'
+
+    message = f'Для подтверждения учетной записи {user.username} на портале \
+    {settings.DOMAIN_NAME} перейдите по ссылке: \n{settings.DOMAIN_NAME}{verify_link}'
+
+    return send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+
 def register(request):
     title = 'Регистрация'
 
     if request.method == 'POST':
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
-
         if register_form.is_valid():
-            register_form.save()
-            return HttpResponseRedirect(reverse('auth:login'))
+            user = register_form.save()
+            if send_verify_mail(user):
+                print('сообщение подтверждения отправлено')
+                return HttpResponseRedirect(reverse('auth:login'))
+            else:
+                print('ошибка отправки сообщения')
+                return HttpResponseRedirect(reverse('auth:login'))
     else:
         register_form = ShopUserRegisterForm()
+        content = {'title': title, 'register_form': register_form}
+        return render(request, 'authapp/register.html', content)
 
-    content = {'title': title, 'register_form': register_form}
 
-    return render(request, 'authapp/register.html', content)
+def verify(request, email, activation_key):
+    title = 'Верификация'
+    content = {'title': title}
+    try:
+        user = ShopUser.objects.get(email=email)
+        if user.activation_key == activation_key and not user.is_activation_key_expired():
+            user.is_active = True
+            user.save()
+            auth.login(request, user)
+            return render(request, 'authapp/verification.html', content)
+        else:
+            print(f'ошибка активации пользователя: {user}')
+            return render(request, 'authapp/verification.html', content)
+    except Exception as e:
+        print(f'ошибка активации пользователя: {e.args}')
+        return HttpResponseRedirect(reverse('main'))
 
 
 def edit(request):
@@ -65,3 +99,5 @@ def edit(request):
     content = {'title': title, 'edit_form': edit_form}
 
     return render(request, 'authapp/edit.html', content)
+
+
